65855a75d349a891e791f74cf4924f7f334c9916
Chase Goodband <cgoodband1@gmail.com>
2021-04-23 09:49:23 -0400

    Update insurance_sorting.java
    added comments

--- insurance_sorting.java
+++ insurance_sorting.java
@@ -3,12 +3,13 @@ import java.io.FileReader;
 import java.util.StringTokenizer;
  
 public class insurance_sorting {
+	//create hashtable object
 	static HashTable h = new HashTable(10);
 
 	public static void main(String[] args) {
 		try
 		{
-			//csv file containing data
+			//read in csv file
 			String strFile = "./insurance.csv";
 			//create BufferedReader to read csv file
 			BufferedReader br = new BufferedReader( new FileReader(strFile));
@@ -22,17 +23,19 @@ public class insurance_sorting {
 				st = new StringTokenizer(strLine, ",");
 				while(st.hasMoreTokens())
 				{
-					//display csv values
+					//break up data into variables
 					String  userID = st.nextToken();
 					String first_name = st.nextToken();
-		    		String last_name = st.nextToken();
+		    			String last_name = st.nextToken();
 					int version = Integer.parseInt(st.nextToken());
 					String insurance_company = st.nextToken();
 					
+					//insert h object into hashtable
 					h.insert(userID, first_name, last_name, version, insurance_company);
 
 
-
+					//display csv values for debugging purposes
+				
 					//System.out.println(userID);
 					//System.out.println(first_name);
 					//System.out.println(last_name);
@@ -40,9 +43,13 @@ public class insurance_sorting {
 					//System.out.println(insurance_company);
 				}
 			}// while
-
+			
+			//display table for dubugging purposes 
 			//h.displayTable();
+			
+			//insert into new csv files
 			h.insertTable();
+			//close buffer reader
 			br.close();
 
 			
